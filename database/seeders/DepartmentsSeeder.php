80f8bdec874eda0b13165aef14e60af3bc227440
xZoru <xzoruuu@gmail.com>
2026-07-22 15:34:19 +0800

    removed seeders for department

--- database/seeders/DepartmentsSeeder.php
+++ database/seeders/DepartmentsSeeder.php
@@ -9,32 +9,6 @@ class DepartmentsSeeder extends Seeder
 {
     public function run()
     {
-        $departments = [
-            'HR',
-            'Finance',
-            'IT',
-            'Operations',
-            'Sales',
-            'Marketing',
-            'Security',
-            'Administration'
-        ];
-
-        foreach ($departments as $dept) {
-            // This now creates exactly 1 universal row per department name
-            Department::firstOrCreate(
-                [
-                    'name' => $dept,
-                    'company_id' => null, // null to make it universal
-                ],
-                [
-                    'code' => 'GLOBAL-' . substr($dept, 0, 3),
-                    'description' => $dept . ' Universal System Department',
-                    'is_active' => true,
-                ]
-            );
-        }
-
         $this->command->info('Universal Departments seeded successfully!');
     }
 }
