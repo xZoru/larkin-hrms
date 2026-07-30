663869620ff785056a3ad54f32f8f5fab31eee7f
xZoru <xzoruuu@gmail.com>
2026-07-30 13:30:34 +0800

    fix

--- resources/views/employees/create.blade.php
+++ resources/views/employees/create.blade.php
@@ -104,7 +104,7 @@
                                 </div>
                                 <div>
                                     <label class="block text-sm font-medium text-gray-700">Employee Number</label>
-                                    <input type="text" name="employee_number" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" value="{{ old('employee_number') }}>
+                                    <input type="text" name="employee_number" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" value="{{ old('employee_number') }}" placeholder="">
                                 </div>
                                 <div>
                                     <label class="block text-sm font-medium text-gray-700 required-label">First Name</label>
