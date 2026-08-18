3b0af266133a845c5182e14336a35e049fcb7efb
xZoru <xzoruuu@gmail.com>
2026-08-18 09:13:43 +0800

    final pay calculations and layout fixes 2

--- resources/views/settlements/final-pay-pdf.blade.php
+++ resources/views/settlements/final-pay-pdf.blade.php
@@ -5,13 +5,27 @@ body { font-family: Arial, sans-serif; font-size: 12px; line-height: 1.3; }
 .copies { width: 100%; border-collapse: collapse; table-layout: fixed; }
 .copies td { width: 50%; vertical-align: top; border: 1px solid #222; padding: 16px; }
 .header { height: 65px; } .logo { max-width: 165px; max-height: 50px; } .right { text-align: right; }
-.name, .amount { font-weight: bold; } .calculation { margin: 28px 0 22px; } .calc td { border: 0; padding: 2px; width: auto; }
+.name, .amount { font-weight: bold; }
+.employee-details { margin: 0 0 0 30px; border-collapse: collapse; line-height: 1.25; }
+.employee-details td { border: 0; padding: 0; } .employee-details .label { width: 118px; } .employee-details .value { font-weight: bold; }
+.calculation { margin: 28px 0 22px; } .calc { margin: 10px 0 0 76px; border-collapse: collapse; } .calc td { border: 0; padding: 2px 0; width: auto; }
+.calc .operator { width: 22px; } .calc .figure { width: 55px; text-align: right; padding-right: 14px; } .calc .description { white-space: nowrap; }
 .total { border-top: 1px solid #222; font-size: 15px; font-weight: bold; } .signature { margin-top: 28px; }
-.line { display: inline-block; width: 150px; text-align: center; border-bottom: 1px solid #222; }
+.issued { text-align: center; } .issuer-position { margin-left: 100px; font-style: italic; } .signature-row { margin: 16px 0 0 20px; }
+.line { display: inline-block; width: 170px; text-align: center; border-bottom: 1px solid #222; }
 </style></head><body>
 <table class="copies"><tr>@for($copy = 0; $copy < 2; $copy++)<td>
     <div class="header">@if($company?->logo_data)<img class="logo" src="{{ $company->logo_data }}">@endif<div class="right">{{ now()->format('F d, Y') }}</div></div>
-    <p>Employee Number: <span class="name">{{ $payment->employee->employee_number }}</span><br>Name: <span class="name">{{ $payment->employee->full_name }}</span><br>Position: {{ $payment->employee->position }}<br>Date Commenced: {{ $payment->commenced_at->format('Y-m-d') }}<br>End Date: {{ $payment->ended_at->format('Y-m-d') }}<br>Hours per day: {{ number_format($payment->hours_per_day, 2) }}<br>Hourly rate: {{ number_format($payment->hourly_rate, 2) }}<br>Final Pay: <span class="name">{{ number_format($payment->amount, 2) }}</span></p>
-    <div class="calculation">Calculation:<table class="calc"><tr><td></td><td>1.5</td></tr><tr><td>x</td><td>{{ number_format($payment->hours_per_day, 2) }}</td><td>Hours/day</td></tr><tr><td>x</td><td>{{ number_format($payment->hourly_rate, 2) }}</td><td>Hourly rate</td></tr><tr><td>x</td><td class="total">{{ number_format($payment->service_months, 2) }}</td><td>Months in Service</td></tr><tr><td></td><td class="total">K {{ number_format($payment->amount, 2) }}</td></tr></table></div>
-    <div class="signature"><b>Issued by:</b> <span class="name">{{ strtoupper($payment->issuer_name) }}</span><br><span style="margin-left:100px;font-style:italic">{{ $payment->issuer_position }}</span><p><b>Received by:</b> <span class="line">{{ $payment->employee->full_name }}</span></p><p><b>Date:</b> <span class="line">&nbsp;</span></p></div>
+    <table class="employee-details">
+        <tr><td class="label">Employee Number:</td><td class="value">{{ $payment->employee->employee_number }}</td></tr>
+        <tr><td class="label">Name:</td><td class="value">{{ $payment->employee->full_name }}</td></tr>
+        <tr><td class="label">Position:</td><td class="value">{{ $payment->employee->position }}</td></tr>
+        <tr><td class="label">Date Commenced:</td><td class="value">{{ $payment->commenced_at->format('Y-m-d') }}</td></tr>
+        <tr><td class="label">End Date:</td><td class="value">{{ $payment->ended_at->format('Y-m-d') }}</td></tr>
+        <tr><td class="label">Hours per day:</td><td class="value">{{ number_format($payment->hours_per_day, 2) }}</td></tr>
+        <tr><td class="label">Hourly rate:</td><td class="value">{{ number_format($payment->hourly_rate, 2) }}</td></tr>
+        <tr><td class="label">Final Pay:</td><td class="value">{{ number_format($payment->amount, 2) }}</td></tr>
+    </table>
+    <div class="calculation">Calculation:<table class="calc"><tr><td class="operator"></td><td class="figure">1.5</td><td class="description"></td></tr><tr><td class="operator">x</td><td class="figure">{{ number_format($payment->hours_per_day, 2) }}</td><td class="description">Hours/day</td></tr><tr><td class="operator">x</td><td class="figure">{{ number_format($payment->hourly_rate, 2) }}</td><td class="description">Hourly rate</td></tr><tr><td class="operator">x</td><td class="figure total">{{ number_format($payment->service_months, 2) }}</td><td class="description">Months in Service</td></tr><tr><td class="operator"></td><td class="figure total">K {{ number_format($payment->amount, 2) }}</td><td class="description"></td></tr></table></div>
+    <div class="signature"><div class="issued"><b>Issued by:</b> <span class="name">{{ strtoupper($payment->issuer_name) }}</span><br><span class="issuer-position">{{ $payment->issuer_position }}</span></div><div class="signature-row"><b>Received by:</b> <span class="line">{{ $payment->employee->full_name }}</span></div><div class="signature-row"><b>Date:</b> <span class="line">&nbsp;</span></div></div>
 </td>@endfor</tr></table></body></html>
